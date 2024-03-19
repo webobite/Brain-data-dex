9053446705fff31a828105846b014108ac5e0e47
Subham Singh <singhsubham510@gmail.com>
2024-03-19 11:30:32 +0000

    Direct with System message and some better Description for tool arguments

--- main.py
+++ main.py
@@ -26,23 +26,28 @@ from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool, list_tables
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
 tables = list_tables()
-print(tables)
 prompt = ChatPromptTemplate(
     messages=[
-        SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -57,5 +62,4 @@ agent_executor = AgentExecutor(
 )
 
 agent_executor("How many users have provided a shipping address?")
-
-# agent_executor("How many users are in the database?")
+# agent_executor("how many users are there?")
