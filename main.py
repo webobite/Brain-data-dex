fb1f27b069aa6e00fffd7df2a96b36d2a2c218c6
Subham Singh <singhsubham510@gmail.com>
2024-03-19 11:55:47 +0000

    Added code for Tools with Multi Arguments for dumping the report in html file

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
-
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -47,7 +47,11 @@ prompt = ChatPromptTemplate(
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+tools = [
+    run_query_tool,
+    describe_tables_tool,
+    write_report_tool
+]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -60,6 +64,13 @@ agent_executor = AgentExecutor(
     verbose=True,
     tools=tools
 )
+agent_executor("How many orders are there? Write the results to a html report.")
 
-agent_executor("How many users have provided a shipping address?")
+agent_executor(
+    "Repeat the exact same process for users."
+)
+
+# agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+
+# agent_executor("How many users have provided a shipping address?")
 # agent_executor("how many users are there?")
