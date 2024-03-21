99150be60074246f7da0f891378a5a7ecba1637c
Subham Singh <singhsubham510@gmail.com>
2024-03-21 15:46:46 +0000

    Added the conversational chat interface connected with the AI agent

--- src/app.py
+++ src/app.py
@@ -1,37 +1,74 @@
 # import packages
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder
+)
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
 
+from langchain.schema import SystemMessage
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor, initialize_agent
+from langchain.memory import ConversationBufferMemory
+
+from dotenv import load_dotenv
+
 import chainlit as cl
+
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+
+load_dotenv()
+
+
+# get list of tables
+tables = list_tables()
 
 @cl.on_chat_start
 async def on_chat_start():
     model = ChatOpenAI(streaming=True)
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-            ),
-            ("human", "{question}"),
+    prompt = ChatPromptTemplate(
+        messages=[
+        SystemMessage(content=(
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
+        )),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
         ]
     )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+
+
+    # intialised memory
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    tools = [
+        run_query_tool,
+        describe_tables_tool
+    ]
+
+    agent = initialize_agent(
+        llm=model,
+        prompt=prompt,
+        tools=tools,
+        memory=memory
+    )
+
+    # agent_executor = AgentExecutor(
+    #     agent=agent,
+    #     verbose=True,
+    #     tools=tools,
+    #     memory=memory
+    # )
+
+    # runnable = prompt | model | StrOutputParser() | agent
+    cl.user_session.set("agent", agent)
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cl.user_session.get("runnable")
-
-    msg = cl.Message(content="")
-
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    ):
-        await msg.stream_token(chunk)
-
-    await msg.send()
+    agent = cl.user_session.get("agent")
+    cb = cl.LangchainCallbackHandler(stream_final_answer=True)
+    await cl.make_async(agent.run)(message.content, callbacks=[cb])
---
+++ src/tools/sql.py
@@ -0,0 +1,47 @@
+import sqlite3
+from pydantic.v1 import BaseModel
+from typing import List
+from langchain.tools import Tool
+
+conn = sqlite3.connect("db.sqlite", check_same_thread=False)
+
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    print(">>>>>>>>DEBUG>>>>>>>>>>", "\n".join(row[0] for row in rows if row[0] is not None))
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+def run_sqlite_query(query):
+    c = conn.cursor()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occured: {str(err)}"
+
+class RunQueryArgsSchema(BaseModel):
+    query: str
+
+run_query_tool = Tool.from_function(
+    name="run_sqlite_query",
+    description="Run a sqlite query.",
+    func=run_sqlite_query,
+    args_schema= RunQueryArgsSchema
+)
+
+class DescribeTablesArgsSchema(BaseModel):
+    tables_names: List[str]
+
+def describe_tables(tables_names):
+    c = conn.cursor()
+    tables = ', '.join("'" + table + "'" for table in tables_names)
+    rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' and name IN({tables});")
+    return '\n'.join(row[0] for row in rows if row[0] is not None)
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of tables names, returns the schema of those tables",
+    func=describe_tables,
+    args_schema=DescribeTablesArgsSchema
+)
